ee8dfd405e66fb919c4822e91a8abbc0949782bc
kanta.yamada <yamada.kanta@lmi.ne.jp>
2020-06-24 07:52:29 +0000

    like

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence:true, length: { maximum: 225 }
+  
+  has_many :favorites
+  has_many :favorited, through: :favorites, source: :user
 end
